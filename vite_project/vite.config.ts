53442b15171dc976030593b3b1e8f79d3bdd8c8f
Herve Tribouilloy <herve@digitalrisedorset.co.uk>
2026-02-02 12:21:37 +0000

    Simplified vite config to enable production to work in CI/CD mode

--- vite_project/vite.config.ts
+++ vite_project/vite.config.ts
@@ -1,50 +1,31 @@
-import {defineConfig, loadEnv} from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
 import pkg from './package.json';
 
-console.log('AUTH BRIDGE TARGET:', process.env.VITE_AUTH_BRIDGE_TARGET);
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  const authBridgeTarget = env.VITE_AUTH_BRIDGE_TARGET;
-
-  if (mode === 'development' && !authBridgeTarget) {
-    throw new Error(
-        'VITE_AUTH_BRIDGE_TARGET is required in development mode'
-    );
-  }
-
-  return {
-    server: {
-      proxy: {
-        '/auth': {
-          target: authBridgeTarget,
-          changeOrigin: true,
-        },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ['widget-booking.local'],
+  },
+  define: {
+    'process.env': {},
+  },
+  build: {
+    outDir: "../www",
+    emptyOutDir: false,
+    lib: {
+      entry: "src/widget.ts",
+      name: "WidgetBookingSystem",
+      fileName: () => `widget-booking@${pkg.version}.iife.js`,
+      formats: ["iife"],
+    },
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+        assetFileNames: "widget-booking.[ext]",
       },
     },
-    plugins: [react()],
-    define: {
-      'process.env': {},
-    },
-    build: {
-      outDir: "../www",
-      emptyOutDir: false,
-      lib: {
-        entry: "src/widget.ts",
-        name: "WidgetBookingSystem",
-        fileName: () => `widget-booking@${pkg.version}.iife.js`,
-        formats: ["iife"],
-      },
-      rollupOptions: {
-        output: {
-          inlineDynamicImports: true,
-          assetFileNames: "widget-booking.[ext]",
-        },
-      },
-      minify: true,
-      sourcemap: false
-    }
+    minify: true,
+    sourcemap: false
   }
 });
